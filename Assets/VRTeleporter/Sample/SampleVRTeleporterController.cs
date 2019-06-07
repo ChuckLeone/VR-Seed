d0f77c02a064f59fd00c372e4accb4a27717b5c7
Chuck Leone <chuckleone@gmail.com>
2019-06-07 00:32:30 -0400

    Initial commit.

---
+++ Assets/VRTeleporter/Sample/SampleVRTeleporterController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus;
+
+public class SampleVRTeleporterController : MonoBehaviour {
+
+    public VRTeleporter teleporter;
+
+	void Update () {
+         if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            teleporter.ToggleDisplay(true);
+        }
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+	}
+}
